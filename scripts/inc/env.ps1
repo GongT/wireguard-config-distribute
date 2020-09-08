682b6166e62e41e3257ec786647f8ec012ddb25c
GongT <admin@gongt.me>
2020-09-08 19:46:20 +0800

    fix no golang
    Former-commit-id: 841b23855483abf49425652dc1bedbcba4d98196

--- scripts/inc/env.ps1
+++ scripts/inc/env.ps1
@@ -2,6 +2,14 @@
 
 Set-Location $PSScriptRoot/../..
 
+Function Test-CommandExists {
+	Param ($command)
+
+	try { if (Get-Command $command) { return $true } }
+
+	Catch { return $false }
+} 
+
 function IsWindows() {
 	$iswin = Get-Variable IsWindows -Scope Global -ErrorAction SilentlyContinue
 	if ($iswin ) {
@@ -17,7 +25,7 @@ git diff-index --quiet HEAD
 if ( $? -eq $false ) { $GH += "[M]" }
 $env:LDFLAGS += " -X '$T.build_date=$(Get-Date -Format "yyyy-MM-dd+HH:mm:ss")' -X '$T.build_git_hash=$GH'"
 
-if ( $(go env GOOS) -Eq "windows" ) {
+if ( ( Test-CommandExists go ) -And ( $(go env GOOS) -Eq "windows" ) ) {
 	# go build -ldflags -H=windowsgui -o dist/$type ./cmd/wireguard-config-$type
 	# $env:GOGCCFLAGS += " -ldflags -H=windowsgui"
 	$env:GOEXE = ".exe"
--- scripts/inc/x.ps1
+++ scripts/inc/x.ps1
@@ -30,7 +30,20 @@ function  SetExecuteMethod() {
 
 		$global:CID = $(podman inspect --type=image '--format={{.Id}}' gongt/wg-config-build)
 		if ($? -ne $true) {
-			$cache = @("--volume=$($env:GOPATH):/root/go")
+			if (-Not $env:GOPATH) {
+				if ( Test-CommandExists go ) {
+					$env:GOPATH = $(go env GOPATH)
+				}
+				if (-Not $env:GOPATH) {
+					$env:GOPATH = New-Item -Force -ItemType "directory" -Path $env:SYSTEM_COMMON_CACHE -Name gopath
+					Write-Output "using dummy GOPATH=${env:GOPATH}"
+				} else {
+					Write-Output "using go env GOPATH=${env:GOPATH}"
+				}
+			} else {
+				Write-Output "using system GOPATH=${env:GOPATH}"
+			}
+			$cache = @("--volume=${env:GOPATH}:/root/go")
 			if ( Test-Path $env:SYSTEM_COMMON_CACHE/apk ) {
 				$cache += "--volume=$env:SYSTEM_COMMON_CACHE/apk:/etc/apk/cache"
 			}
